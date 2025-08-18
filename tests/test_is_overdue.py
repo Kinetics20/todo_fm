df117e77cf6ba1442e13650e17533b81517f11e5
Kinetics20 <kinetics20@gmail.com>
2025-08-18 16:35:37 +0200

    test_is_overdue

--- tests/test_is_overdue.py
+++ tests/test_is_overdue.py
@@ -10,17 +10,25 @@ def test_is_overdue_due_in_past_not_done(fixed_today, base_task) -> None:
     assert is_overdue(task, today=fixed_today) is True
 
 
-def test_is_overdue_due_in_past_but_done():
-    pass
+def test_is_overdue_due_in_past_but_done(fixed_today, base_task):
+    task = cast(Task, {**base_task, "due_date": fixed_today - timedelta(days=5), "done": True})
+
+    assert is_overdue(task, today=fixed_today) is False
 
 
-def test_is_overdue_due_today_not_done():
-    pass
+def test_is_overdue_due_today_not_done(fixed_today, base_task):
+    task = cast(Task, {**base_task, "due_date": fixed_today, "done": False})
+
+    assert is_overdue(task, today=fixed_today) is False
 
 
-def test_is_overdue_due_in_future_not_done():
-    pass
+def test_is_overdue_due_in_future_not_done(fixed_today, base_task):
+    task = cast(Task, {**base_task, "due_date": fixed_today + timedelta(days=5), "done": False})
+
+    assert is_overdue(task, today=fixed_today) is False
 
 
-def test_is_overdue_no_due_date():
-    pass
+def test_is_overdue_no_due_date(fixed_today, base_task):
+    task = cast(Task, {**base_task, "due_date": None, "done": False})
+
+    assert is_overdue(task, today=fixed_today) is False
