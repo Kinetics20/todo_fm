0d3c6659ce10e27c2ec4e3e6829ed94848b0f794
Kinetics20 <kinetics20@gmail.com>
2025-08-19 17:38:28 +0200

    tests

--- conftest.py
+++ conftest.py
@@ -9,8 +9,8 @@ from src.task import Priority, Task
 
 @pytest.fixture
 def fixed_today() -> date:
-    """Constant value for tests" (2025-08-15)."""
-    return date(2025, 8, 18)
+    """Constant value for tests" (2025-08-19)."""
+    return date(2025, 8, 19)
 
 
 @pytest.fixture
--- tests/test_time_left.py
+++ tests/test_time_left.py
@@ -8,9 +8,13 @@ def test_time_left_done_returns_completed(base_task: Task, fixed_today: date) ->
     assert time_left(task, today=fixed_today) == "Task already completed."
 
 
-# test_time_left_infinite_when_no_due_date
-# test_time_left_overdue_plural
-# test_time_left_overdue_singular
+def test_time_left_infinite_when_no_due_date(base_task: Task, fixed_today: date) -> None:
+    task = cast(Task, ({**base_task, 'due_date': None}))
+    assert time_left(task, today=fixed_today) == "Infinite time left."
+
+
+# test_time_left_overdue_plural @one day overdue
+# test_time_left_overdue_singular @more than one day overdue
 # test_time_left_due_today
 # test_time_left_future_plural
 # test_time_left_future_singular
