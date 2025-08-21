4c3b1a5220b937b3644b511be72bdd9c7486a3c4
Kinetics20 <kinetics20@gmail.com>
2025-08-21 16:03:52 +0200

    conftest

--- conftest.py
+++ conftest.py
@@ -9,8 +9,8 @@ from src.task import Priority, Task
 
 @pytest.fixture
 def fixed_today() -> date:
-    """Constant value for tests" (2025-08-20)."""
-    return date(2025, 8, 20)
+    """Constant value for tests" (2025-08-21)."""
+    return date(2025, 8, 21)
 
 
 @pytest.fixture
