6bde5f400f987561f177d96dfa71f5dc6acb0f9e
Kinetics20 <kinetics20@gmail.com>
2025-08-22 14:12:07 +0200

    test_time_lift

--- tests/test_time_left.py
+++ tests/test_time_left.py
@@ -14,26 +14,25 @@ def test_time_left_infinite_when_no_due_date(base_task: Task, fixed_today: date)
 
 
 def test_time_left_overdue_plural(base_task: Task, fixed_today: date) -> None:
-
     task = cast(Task, ({**base_task, 'due_date': fixed_today - timedelta(days=1), "done": False}))
     assert time_left(task, today=fixed_today) == f"Overdue by 1 day."
 
 
 def test_time_left_overdue_singular(base_task: Task, fixed_today: date) -> None:
-
     task = cast(Task, ({**base_task, 'due_date': fixed_today - timedelta(days=3), "done": False}))
     assert time_left(task, today=fixed_today) == f"Overdue by {(timedelta(days=3)).days} days."
 
 
 def test_time_left_due_today(base_task: Task, fixed_today: date) -> None:
-
     task = cast(Task, ({**base_task, 'due_date': fixed_today, "done": False}))
     assert time_left(task, today=fixed_today) == f"Due today!"
 
-def test_time_left_future_singular(base_task: Task, fixed_today: date) -> None:
 
+def test_time_left_future_singular(base_task: Task, fixed_today: date) -> None:
     task = cast(Task, ({**base_task, 'due_date': fixed_today + timedelta(days=1), "done": False}))
     assert time_left(task, today=fixed_today) == f"1 day left."
 
 
-# test_time_left_future_plural
+def test_time_left_future_plural(base_task: Task, fixed_today: date) -> None:
+    task = cast(Task, ({**base_task, 'due_date': fixed_today + timedelta(days=10), "done": False}))
+    assert time_left(task, today=fixed_today) == f"10 days left."
