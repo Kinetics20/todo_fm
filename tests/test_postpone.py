ae950c3d07585a54f3ff0ae00eca817a3c7dcd73
Kinetics20 <kinetics20@gmail.com>
2025-09-03 15:38:20 +0200

    test_postpone

--- tests/test_postpone.py
+++ tests/test_postpone.py
@@ -3,7 +3,7 @@ from datetime import date, timedelta
 from src.task import Task, postpone
 
 
-def test_postpone_days_zero(base_task: Task, fixed_today: date) -> None:
+def test_postpone_days_equal_zero(base_task: Task, fixed_today: date) -> None:
     postpone(base_task, 0, today=fixed_today)
 
     assert base_task['due_date'] == fixed_today + timedelta(days=2)
