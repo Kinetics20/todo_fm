f33655b0e13566e0c60a3e639f5e94d8e32ede12
Kinetics20 <kinetics20@gmail.com>
2025-09-08 23:32:24 +0200

    added test suit for normalize_status

---
+++ tests/test_normalize_status.py
@@ -0,0 +1,23 @@
+import re
+
+import pytest
+
+from src.task import StatusEnum, normalize_status
+
+
+@pytest.mark.parametrize(
+    "status, expected_status",
+    [
+        (StatusEnum.COMPLETED, StatusEnum.COMPLETED),
+        (2, StatusEnum.COMPLETED),
+        (4, StatusEnum.OVERDUE),
+    ],
+)
+def test_normalize_status_valid(status: StatusEnum | int, expected_status: StatusEnum) -> None:
+    assert normalize_status(status) == expected_status
+
+
+def test_normalize_status_invalid() -> None:
+    pattern = f"Status must be one of: {[s.value for s in StatusEnum]}"
+    with pytest.raises(ValueError, match=re.escape(pattern)):
+        normalize_status(2000)
