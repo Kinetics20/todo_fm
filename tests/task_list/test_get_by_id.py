ff69f08240f4bc7b5207f1a18d34b6535acaddb8
Kinetics20 <kinetics20@gmail.com>
2025-09-22 22:36:51 +0200

    corrected implementation for get_by_id_exist test case

--- tests/task_list/test_get_by_id.py
+++ tests/task_list/test_get_by_id.py
@@ -1,13 +1,12 @@
 import pytest
 
-from src.task import Task
 from src.task_list import TaskList, get_by_id
 
 
-def test_get_by_id_exist(base_task_list: TaskList, base_task: Task) -> None:
-    get_by_id(base_task_list, "1")
-    expected_task = next(task for task in base_task_list if task["id"] == "1")
-    assert expected_task == base_task
+def test_get_by_id_exist(base_task_list: TaskList) -> None:
+    task = get_by_id(base_task_list, "1")
+
+    assert task["id"] == "1"
 
 
 def test_get_by_id_not_exist(base_task_list: TaskList) -> None:
