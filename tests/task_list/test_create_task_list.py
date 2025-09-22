104850bec4de3d3842afebdbb52606713570176c
Kinetics20 <kinetics20@gmail.com>
2025-09-22 22:35:30 +0200

    added raises_on_duplicate test case

--- tests/task_list/test_create_task_list.py
+++ tests/task_list/test_create_task_list.py
@@ -1,3 +1,6 @@
+import pytest
+
+from src.task import Task
 from src.task_list import TaskList, create_task_list
 
 
@@ -7,3 +10,8 @@ def test_create_task_list_empty() -> None:
 
 def test_create_task_list_from_iterable(base_task_list: TaskList) -> None:
     assert create_task_list(base_task_list) == base_task_list
+
+
+def test_create_task_list_raises_on_duplicate(base_task: Task) -> None:
+    with pytest.raises(ValueError, match="Can not be create task list because it contains duplicates."):
+        create_task_list([base_task, base_task])
