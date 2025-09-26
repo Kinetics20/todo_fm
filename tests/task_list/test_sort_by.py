e2170a1a6c1cf96432aa6925c66e0d1a7613055c
Kinetics20 <kinetics20@gmail.com>
2025-09-26 22:27:39 +0200

    added type ignore[arg-type] for sort_by_invalid_by test case

--- tests/task_list/test_sort_by.py
+++ tests/task_list/test_sort_by.py
@@ -4,42 +4,49 @@ from src.task import Task
 from src.task_list import sort_by, TaskList
 
 
-def test_sort_by_priority(base_task: Task, base_task_priority_low: Task) -> None:
-    my_task_list = [base_task, base_task_priority_low]
+def test_sort_by_priority(base_task: Task, task_priority_low: Task) -> None:
+    my_task_list = [base_task, task_priority_low]
     result = sort_by(my_task_list, by="priority")
 
-    assert result == [base_task_priority_low, base_task]
+    assert result == [task_priority_low, base_task]
 
 
-def test_sort_by_priority_reverse_true(base_task: Task, base_task_priority_low: Task) -> None:
-    my_task_list = [base_task, base_task_priority_low]
+def test_sort_by_priority_reverse_true(base_task: Task, task_priority_low: Task) -> None:
+    my_task_list = [base_task, task_priority_low]
     result = sort_by(my_task_list, by="priority", reverse=True)
 
-    assert result == [base_task, base_task_priority_low]
+    assert result == [base_task, task_priority_low]
 
 
-def test_sort_by_description(base_task: Task, base_task_priority_low: Task) -> None:
-    my_task_list = [base_task, base_task_priority_low]
+def test_sort_by_description(base_task: Task, task_priority_low: Task) -> None:
+    my_task_list = [base_task, task_priority_low]
     result = sort_by(my_task_list, by="description")
 
-    assert result == [base_task_priority_low, base_task]
+    assert result == [task_priority_low, base_task]
 
 
-def test_sort_by_due_date(base_task: Task, base_task_priority_low: Task) -> None:
-    my_task_list = [base_task, base_task_priority_low]
+def test_sort_by_status(base_task: Task, task_priority_low: Task) -> None:
+    my_task_list = [task_priority_low, base_task]
+    result = sort_by(my_task_list, by='status')
+
+    assert result == [base_task, task_priority_low]
+
+
+def test_sort_by_due_date(base_task: Task, task_priority_low: Task) -> None:
+    my_task_list = [base_task, task_priority_low]
     result = sort_by(my_task_list, by="due_date")
 
-    assert result == [base_task, base_task_priority_low]
+    assert result == [base_task, task_priority_low]
 
 
-def test_sort_by_created_at(base_task: Task, base_task_priority_low: Task) -> None:
-    my_task_list = [base_task, base_task_priority_low]
+def test_sort_by_created_at(base_task: Task, task_priority_low: Task) -> None:
+    my_task_list = [base_task, task_priority_low]
     result = sort_by(my_task_list, by="created_at")
 
-    assert result == [base_task_priority_low, base_task]
+    assert result == [task_priority_low, base_task]
 
 
 def test_sort_by_invalid_by(base_task_list: TaskList) -> None:
     by = 'family'
     with pytest.raises(ValueError, match=f"Unsupported sort key: {by}"):
-        sort_by(base_task_list, by=by)
+        sort_by(base_task_list, by=by)  # type: ignore[arg-type]
